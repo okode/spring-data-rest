75f439eaf602278b11dab7ae000b5566f3084af2
Pedro Jorquera <pjorquera@okode.com>
2015-12-21 20:56:45 +0100

    Added support for listing derived types from base collection

--- src/main/java/com/okode/crm/repository/InvoiceRepository.java
+++ src/main/java/com/okode/crm/repository/InvoiceRepository.java
@@ -1,9 +1,15 @@
 package com.okode.crm.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.okode.crm.model.Invoice;
 
 public interface InvoiceRepository extends MongoRepository<Invoice, String> {
+	
+	@Query("{ '_class': 'invoice' }")
+	Page<Invoice> findAll(Pageable pageable);
 	
 }
